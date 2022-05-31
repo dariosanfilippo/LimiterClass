b61a84ac148cbe00f984cbe0906bc92311c2ab46
Dario Sanfilippo <sanfilippo.dario@gmail.com>
2022-06-01 01:19:17 +0200

    safety guards and minor changes

--- ExpSmootherCascade.hpp
+++ ExpSmootherCascade.hpp
@@ -10,24 +10,29 @@
 #pragma once
 
 #include <cmath>
+#include <limits>
 
 template<size_t stages, typename real>
 class ExpSmootherCascade {
+    
+    static_assert(stages > 0, "The ExpSmootherCascade class expects one or more stages.");
+    
     private:
-        real SR = 48000.0; // Samplerate as a float variable for later calculations.
-        real T = 1.0 / SR; // Sampling period.
-        const real twoPi = 2.0 * M_PI;
-        real twoPiT = twoPi * T;
-        real attTime = .001; // Attack time in seconds.
-        real relTime = .01; // Release time in seconds.
     
         /* Coefficient correction factor to maintain consistent attack and
          * decay rates when cascading multiple one-pole sections. */
         const real coeffCorrection =
             1.0 / std::sqrt(std::pow(2.0, 1.0 / real(stages)) - 1.0);
     
-        real attCoeff = std::exp((-twoPiT * coeffCorrection) / attTime);
-        real relCoeff = std::exp((-twoPiT * coeffCorrection) / relTime);
+        const real epsilon = std::numeric_limits<real>::epsilon();
+        real SR = 48000.0; // Samplerate as a float variable for later calculations.
+        real T = 1.0 / SR; // Sampling period.
+        const real twoPiC = 2.0 * M_PI * coeffCorrection;
+        real twoPiCT = twoPiC * T;
+        real attTime = .001; // Attack time in seconds.
+        real relTime = .01; // Release time in seconds.
+        real attCoeff = std::exp(-twoPiCT / attTime);
+        real relCoeff = std::exp(-twoPiCT / relTime);
     
         /* We store the coefficients in an array for efficient Boolean
          * fetching without branching. */
@@ -49,9 +54,9 @@ class ExpSmootherCascade {
 
 template<size_t stages, typename real>
 void ExpSmootherCascade<stages, real>::SetSR(real _SR) {
-    SR = _SR;
+    SR = std::max<real>(1.0, _SR);
     T = 1.0 / SR;
-    twoPiT = twoPi * T;
+    twoPiCT = twoPiC * T;
 }
 
 /* We store attack and relelease phases coefficients in an array for look-up 
@@ -59,15 +64,15 @@ void ExpSmootherCascade<stages, real>::SetSR(real _SR) {
  * multiplications by bools. */
 template<size_t stages, typename real>
 void ExpSmootherCascade<stages, real>::SetAttTime(real _attTime) {
-    attTime = _attTime;
-    attCoeff = std::exp((-twoPiT * coeffCorrection) / attTime);
+    attTime = std::max<real>(epsilon, _attTime);
+    attCoeff = std::exp(-twoPiCT / attTime);
     coeff[1] = attCoeff;
 }
 
 template<size_t stages, typename real>
 void ExpSmootherCascade<stages, real>::SetRelTime(real _relTime) {
-    relTime = _relTime;
-    relCoeff = std::exp((-twoPiT * coeffCorrection) / relTime);
+    relTime = std::max<real>(epsilon, _relTime);
+    relCoeff = std::exp(-twoPiCT / relTime);
     coeff[0] = relCoeff;
 }
 
@@ -75,7 +80,7 @@ void ExpSmootherCascade<stages, real>::SetRelTime(real _relTime) {
  * samples of the input signal and stores it in the output vector. */
 template<size_t stages, typename real>
 void ExpSmootherCascade<stages, real>::Process(real* xVec, real* yVec, size_t vecLen) {
-    for (size_t n = 0; n < vecLen; n++) { // level-0 for-loop
+    for (size_t n = 0; n < vecLen; n++) { // Level-0 for-loop.
         
         /* Outside of the inner for-loop, we assign the input vector sample 
          * to an auxiliary variable, which will be the input to the first 
@@ -83,7 +88,7 @@ void ExpSmootherCascade<stages, real>::Process(real* xVec, real* yVec, size_t ve
         real input = xVec[n];
 
         /* Compute M series exponential smoothers in an inner for-loop. */
-        for (size_t stage = 0; stage < stages; stage++) { // level-1 for-loop
+        for (size_t stage = 0; stage < stages; stage++) { // Level-1 for-loop.
 
             /* Determine whether the system is in the attack or release 
              * phase, namely checking if the input is greater than the 
@@ -99,13 +104,13 @@ void ExpSmootherCascade<stages, real>::Process(real* xVec, real* yVec, size_t ve
              * output of the current one. */
             input = output[stage];
 
-        } // end of level-1 for-loop
+        } // End of level-1 for-loop.
 
         /* Finally, we assign the output of the last section to the output
          * vector sample. */
         yVec[n] = output[stages - 1];
 
-    } // end of level-0 for-loop
+    } // End of level-0 for-loop.
 }
 
 /* We store attack and relelease phases coefficients in an array for look-up
@@ -113,13 +118,13 @@ void ExpSmootherCascade<stages, real>::Process(real* xVec, real* yVec, size_t ve
  * multiplications by bools. */
 template<size_t stages, typename real>
 ExpSmootherCascade<stages, real>::ExpSmootherCascade(real _SR, real _attTime, real _relTime) {
-    SR = _SR;
+    SR = std::max<real>(1.0, _SR);
     T = 1.0 / SR;
-    twoPiT = twoPi * T;
-    attTime = _attTime;
-    attCoeff = std::exp((-twoPiT * coeffCorrection) / attTime);
-    relTime = _relTime;
-    relCoeff = std::exp((-twoPiT * coeffCorrection) / relTime);
+    twoPiCT = twoPiC * T;
+    attTime = std::max<real>(epsilon, _attTime);
+    relTime = std::max<real>(epsilon, _relTime);
+    attCoeff = std::exp(-twoPiCT / attTime);
+    relCoeff = std::exp(-twoPiCT / relTime);
     coeff[0] = relCoeff;
     coeff[1] = attCoeff;
 }
